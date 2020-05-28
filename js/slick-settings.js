0620eaa2902a6897db0eb66747186cea48856450
soniaarl <59911419+soniaarl@users.noreply.github.com>
2020-05-27 22:00:49 -0700

    added slick slider for testimonial

---
+++ js/slick-settings.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($){
+
+    // Enable Slick Slider on elements with the class 'slider'
+    $('.slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+
+});
--- template-parts/testimonials.php
+++ template-parts/testimonials.php
@@ -6,11 +6,15 @@
 
 			$query = new WP_Query( $args );
 
-			if ( $query -> have_posts() ){
+			if ( $query -> have_posts() ){ ?>
+                <div class="slider"> <?php
 				while ( $query -> have_posts() ) {
-                    $query -> the_post();
+                    $query -> the_post();?>
+                    <div> <?php
                     the_post_thumbnail();
-					the_content();
-				}
+					the_content(); ?>
+                    </div> <?php
+				} ?>
+                </div><!-- end of slider --> <?php
 				wp_reset_postdata();
 			} 
