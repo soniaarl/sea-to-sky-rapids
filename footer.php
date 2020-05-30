58c5989113db4783e06d5fcb89ab5dd7fb195911
Faris <fasic@my.bcit.ca>
2020-05-30 14:06:56 -0700

    footer update

--- footer.php
+++ footer.php
@@ -13,17 +13,11 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'seatosky' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'seatosky' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'seatosky' ), 'seatosky', '<a href="http://underscores.me/">Kailee, Sonia, Faris, Zahra, Jarrod</a>' );
-				?>
+		<div class="footer-menus">
+			<nav id="footer-navigation" class="footer-navigation">
+        		<?php wp_nav_menu( array( 'theme_location' => 'footer') ); ?>
+    		</nav>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
