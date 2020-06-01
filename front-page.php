c090c6e214769520f68d520c11b60a7870cc586e
Zahra Nouri <zahra.nouri.ic@gmail.com>
2020-06-01 13:07:42 -0700

    adding products to fornt-page.php

--- front-page.php
+++ front-page.php
@@ -24,12 +24,81 @@ get_header();
 			get_template_part( 'template-parts/content', 'page' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+
+			//I have commented the next 3 lines for now -- Zahra
+			// if ( comments_open() || get_comments_number() ) :
+			// 	comments_template();
+			// endif;
 
 		endwhile; // End of the loop.
 		?>
+
+		<?php
+		$args = array(
+			'post_type'			=>'product',
+				'posts_per_page' => 4
+			); 
+
+		$product_query = new WP_Query($args); 
+		if ($product_query->have_posts()){
+			while ($product_query->have_posts()) {
+				$product_query->the_post(); 
+				
+				echo '<article class="front-news">';
+				echo '<a href="'.get_permalink() . '">';
+				the_post_thumbnail('medium');
+				echo '</a>';
+				echo '<p>'. get_the_title() .'</p>';
+				?>
+				 <div class="duration">
+				 
+				 <p> Duration: </p>
+				<?php  echo '<p>'. get_the_content() .'</p>';  ?>
+				</div>
+
+				 </div> 
+				
+				<div class="difficulty_level">
+				
+				<p> difficulty Level: </p>
+				<?php   ?>
+				</div>
+
+				<div class="Age_range">
+				
+				<p> Age Range: </p>
+				<?php   ?>
+
+				</div>
+
+				<div class="min_weight">
+
+				<p> Minimum Weight: </p>
+				<?php   ?>
+
+				</div>
+
+				<div class="price">
+				
+				<p> Price: </p>
+				<?php   ?>
+
+				</div>
+
+
+				<?php
+			
+				echo '</article>';
+			}
+			wp_reset_postdata();
+		} 
+		
+		?>
+		
+
+
+
+
 
 	</main><!-- #main -->
 
@@ -42,5 +111,4 @@ get_header();
 ?>
 
 <?php
-get_sidebar();
 get_footer();
