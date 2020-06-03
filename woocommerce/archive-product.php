18ef86dc3e4d63f82e25d7cfc5d3cd32e8e7fb17
kgiles <info@kgiles.com>
2020-06-03 11:58:03 -0700

    Updated our tours banner error
    Our tours page banner is now only displaying on the Our Tours page.

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -26,7 +26,7 @@ get_header( 'shop' );
  * @hooked woocommerce_breadcrumb - 20
  * @hooked WC_Structured_Data::generate_website_data() - 30
  */
-do_action( 'woocommerce_before_main_content' );
+do_action( 'woocommerce_before_main_content_archive' );
 
 ?>
 <header class="woocommerce-products-header">
