807d79034b00ed2504ad20a87921dfdc8606acf2
kgiles <info@kgiles.com>
2020-06-03 23:03:37 -0700

    Updated content-our-tours ifs and whiles
    KG commit - Updated content-our-tours.php to use if : and while : instead of if { and while {

--- template-parts/content-our-tours.php
+++ template-parts/content-our-tours.php
@@ -17,48 +17,48 @@
 				<h2><?php the_title(); ?></h2>
 		
 				<?php // support for ACF
-				if( function_exists( 'get_field' ) ) :
-					if( have_rows('product_summary') ):
+				if ( function_exists( 'get_field' ) ) :
+					if ( have_rows('product_summary') ):
 						while ( have_rows('product_summary') ) : the_row();
-							if(get_sub_field('duration') ){ ?>
+							if(get_sub_field('duration') ) : ?>
 								
 								<div class='tour-preview-element sts-tour-duration'>
 									<p><b>Duration: </b><?php the_sub_field( 'duration' ); ?>h</p>
 								</div><!-- end of sts-tour-duration -->
 
-							<?php } // end of duration if
+							<?php endif; // end of duration if
 
-							if(get_sub_field('difficulty_level') ){ ?>
+							if (get_sub_field('difficulty_level') ) : ?>
 								
 								<div class='tour-preview-element sts-tour-difficulty_level'>
 									<p><b>Difficulty Level: </b><?php the_sub_field( 'difficulty_level' ); ?></p>
 								</div><!-- end of sts-tour-difficulty_level -->
 
-							<?php } // end of difficulty_level if
+							<?php endif; // end of difficulty_level if
 
-							if(get_sub_field('age_range') ){ ?>
+							if (get_sub_field('age_range') ) : ?>
 								
 								<div class='tour-preview-element sts-tour-age_range'>
 									<p><b>Age Range: </b><?php the_sub_field( 'age_range' ); ?></p>
 								</div> <!-- end of sts-tour-age_range -->
 
-							<?php } // end of age_range if
+							<?php endif; // end of age_range if
 
-							if(get_sub_field('minimum_weight') ){ ?>
+							if (get_sub_field('minimum_weight') ) : ?>
 								
 								<div class='tour-preview-element sts-tour-minimum_weight'>
 									<p><b>Minimum Weight: </b><?php the_sub_field( 'minimum_weight' ); ?>lbs</p>
 								</div><!-- end of sts-tour-minimum_weight -->
 
-							<?php } // end of minimum_weight if
+							<?php endif; // end of minimum_weight if
 
-							if(get_sub_field('price') ){ ?>
+							if (get_sub_field('price') ) : ?>
 								
 								<div class='tour-preview-element sts-tour-price'>
 									<p><b>Price: </b>$<?php the_sub_field( 'price' ); ?></p>
 								</div><!-- // end of sts-tour-price -->
 
-							<?php } // end of price if
+							<?php endif; // end of price if
 
 						endwhile;
 
