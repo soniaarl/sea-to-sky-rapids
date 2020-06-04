60807528ad01ab11556505b64ce7dea8e2cad67d
J Sterling <jsterling9@my.bcit.ca>
2020-06-04 15:33:23 -0700

    update page-staff.php V3

--- page-staff.php
+++ page-staff.php
@@ -14,45 +14,43 @@
 get_header();
 ?>
 
-<div class="who-we-are-banner">
-	<?php while ( have_posts() ) : the_post();
-	the_post_thumbnail( 'full' ); ?>
-	<header class="page-header">
-		<?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
-	</header>
-</div><!-- end who-we-are-banner -->
-
-<main id="primary" class="site-main">
-	<p><?php the_content(); ?></p>
-	
-	<?php get_template_part( 'template-parts/testimonials'); ?>
-
-	<h2>Meet The Team</h2>
-	<?php
-		$args = array (
-			'post_type' => 'sts-staff',
-			'posts_per_page' => -1,
-		);
-		$staff_query = new WP_Query( $args );
-		if ( $staff_query->have_posts() ): ?>
-			<div class="staff-card">
-				<?php while ( $staff_query->have_posts() ):
-					$staff_query->the_post() ?>
-					<?php the_post_thumbnail('large'); ?>
-					<h3 class="staff-name"><?php the_title() ?> </h3>
-					<p class="staff-position"><?php the_field('position'); ?> </p>
-					<p class="field-label">Employee Since</p>
-					<p class="staff-employee"><?php the_field('employee_since'); ?> </p>
-					<p class="field-label">Hometown</p>
-					<p class="staff-hometown"><?php the_field('home_town'); ?> </p>
-					<p class="field-label">About</p>
-					<p class="staff-bio"><?php the_field('bio'); ?> </p>
-				<?php endwhile; ?>
-				<?php wp_reset_postdata(); ?>
-			</div>
-		<?php endif; ?>
-		<?php endwhile; ?>
-</main>
+<?php while ( have_posts() ) : the_post(); ?>
+    <div class="who-we-are-banner">
+        <?php the_post_thumbnail( 'full' ); ?>
+        <header class="page-header">
+            <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
+        </header>
+    </div><!-- end who-we-are-banner -->
+    <main id="primary" class="site-main">
+        <?php the_content(); ?>
+        <?php get_template_part( 'template-parts/testimonials'); ?>
+        
+        <?php
+            $args = array (
+                'post_type' => 'sts-staff',
+                'posts_per_page' => -1,
+            );
+            $staff_query = new WP_Query( $args );
+            if ( $staff_query->have_posts() ): ?>
+            	<h2>Meet The Team</h2>
+                <div class="staff-card">
+                    <?php while ( $staff_query->have_posts() ):
+                        $staff_query->the_post() ?>
+                        <?php the_post_thumbnail('large'); ?>
+                        <h3 class="staff-name"><?php the_title() ?> </h3>
+                        <p class="staff-position"><?php the_field('position'); ?> </p>
+                        <p class="field-label">Employee Since</p>
+                        <p class="staff-employee"><?php the_field('employee_since'); ?> </p>
+                        <p class="field-label">Hometown</p>
+                        <p class="staff-hometown"><?php the_field('home_town'); ?> </p>
+                        <p class="field-label">About</p>
+                        <p class="staff-bio"><?php the_field('bio'); ?> </p>
+                    <?php endwhile; ?>
+                    <?php wp_reset_postdata(); ?>
+                </div>
+            <?php endif; ?>
+    </main>
+<?php endwhile; ?>
 
 <?php
 get_footer();
