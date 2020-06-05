2f5faf0fe5137e2130c1aea045f9cb3320a5fe58
soniaarl <59911419+soniaarl@users.noreply.github.com>
2020-06-04 20:09:42 -0700

    deleted spacing in testimonials.php

--- template-parts/testimonials.php
+++ template-parts/testimonials.php
@@ -1,21 +1,21 @@
 <?php 
 
-			$args = array(
-				'post_type' => 'sts-testimonials',
-				'posts_per_page' => -1
-			);
+$args = array(
+	'post_type' => 'sts-testimonials',
+	'posts_per_page' => -1
+);
 
-			$query = new WP_Query( $args );
+$query = new WP_Query( $args );
 
-			if ( $query -> have_posts() ) : ?>
-                <div class="slider"> <?php
-				while ( $query -> have_posts() ) :
-                    $query -> the_post();?>
-                    <div class="testimonial-container"> <?php
-                    the_post_thumbnail();
-					the_content(); ?>
-                    </div> <?php
-				endwhile; ?>
-                </div><!-- end of slider --> <?php
-				wp_reset_postdata();
-			endif;
+if ( $query -> have_posts() ) : ?>
+	<div class="slider"> <?php
+	while ( $query -> have_posts() ) :
+		$query -> the_post();?>
+		<div class="testimonial-container"> <?php
+		the_post_thumbnail();
+		the_content(); ?>
+		</div> <?php
+	endwhile; ?>
+	</div><!-- end of slider --> <?php
+	wp_reset_postdata();
+endif;
