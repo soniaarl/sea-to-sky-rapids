08df1203c8fd6e1b4982e915982f6977c47552c3
kgiles <info@kgiles.com>
2020-06-04 17:06:31 -0700

    Banner on 'our tours' page now inside main tag
    KG commit

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -31,7 +31,8 @@ do_action( 'woocommerce_before_main_content_archive' );
 ?>
 <main>
 <header class="woocommerce-products-header">
-	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+	<?php echo get_the_post_thumbnail(get_option( 'woocommerce_shop_page_id') );
+	if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
 	<?php endif; ?>
 
