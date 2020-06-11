02069e03c38de7d21b3e309e1247c8f70b90866f
kgiles <info@kgiles.com>
2020-06-11 14:18:15 -0700

    Contact headings styles
    KG commit - All headings on contact page now styled.

--- page-contact.php
+++ page-contact.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 
-<main id="primary" class="site-main">
+<main id="primary" class="site-main contact">
 
 	<div class="contact-banner">
 		<?php while ( have_posts() ) : the_post(); 
