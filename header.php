10fff37830b62dd505e57d8387942a6af64b6e73
Faris <fasic@my.bcit.ca>
2020-06-15 23:02:28 -0700

    mobile header navbar

--- header.php
+++ header.php
@@ -50,7 +50,7 @@
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
 			<span class="screen-reader-text"><?php esc_html_e( 'Menu', 'seatosky' ); ?></span>
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-						<path d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/>
+						<path style="fill:#FFFFFF;" d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"/>
 					</svg>
 		</button>
 			<?php
