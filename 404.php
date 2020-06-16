e0f3bacd0755dde8538f5bda3d20c2ae8a8362ac
kgiles <info@kgiles.com>
2020-06-15 22:44:58 -0700

    404, cart, and checkout styling
    KG commit

--- 404.php
+++ 404.php
@@ -14,42 +14,12 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'seatosky' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Oops! Looks like you got steered the wrong way.', 'seatosky' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'seatosky' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'seatosky' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$seatosky_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'seatosky' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$seatosky_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+				<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'seatosky' ); ?></p>
+				<p>Back to <a href="<?php echo esc_url( home_url( '/' ) ); ?>">home</a>.</p>
 
 			</div><!-- .page-content -->
 		</section><!-- .error-404 -->
